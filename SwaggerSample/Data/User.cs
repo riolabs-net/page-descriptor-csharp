f26d49e893b83575f716690afed16a1246326c4f
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2023-09-07 11:59:58 +0200

    added f def

--- SwaggerSample/Data/User.cs
+++ SwaggerSample/Data/User.cs
@@ -1,40 +1,31 @@
 ﻿using Riolabs.PageDescriptor.Swashbuckle;
 using Riolabs.PageDescriptor.Swashbuckle.Attributes.Fields;
-using Riolabs.PageDescriptor.Swashbuckle.Attributes.Forms;
 using System.ComponentModel.DataAnnotations;
 
 namespace SwaggerSample.Data;
 
-[CustomField("place-birth", "string", "")]
-[CustomField("address", "string", "")]
 public class User
 {
     public int Id { get; set; }
 
     [Required]
     [DataType(DataType.Text)]
-    [Sort(0)]
     public string FirstName { get; set; }
 
     [Required]
     [DataType(DataType.Text)]
-    [Sort(1)]
     public string LastName { get; set; }
 
     [DataType(DataType.EmailAddress)]
     [EmailAddress]
     [Required]
-    [Sort(2)]
     public string Email { get; set; }
 
-    [Classes("date-picker")]
     [DataType(DataType.Date)]
-    [Sort(3)]
     public DateTime BirthDate { get; set; }
 
     [DataType(DataType.Text)]
     [OptionsSimple("Woman", "Man", "Transgender", "Non-binary", "Not-respond")]
-    [Sort(4)]
     public string Gender { get; set; }
 
     [DataType(DataType.Text)]
@@ -44,4 +35,7 @@ public class User
     [DataType(DataType.Text)]
     [OptionsApi("/cars", "GET", "key", "value")]
     public string DrivenCar { get; set; }
+
+    [FieldDefinitions("<GET> /cars")]
+    public Dictionary<string, object> AdditionalProperty { get; set; }
 }
---
+++ riolabs.page-descriptor.swashbuckle/Attributes/Fields/FieldDefinitionsAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Riolabs.PageDescriptor.Swashbuckle.Attributes.Fields;
+public class FieldDefinitionsAttribute : FormInfoAttribute
+{
+    private string Method { get; set; }
+    private string Path { get; set; }
+    public FieldDefinitionsAttribute(string options)
+    {
+        string pattern = @"<([^>]+)>\s+(.+)";
+        List<ITextValue> optionsList = new();
+        Match match = Regex.Match(options, pattern);
+        if (match.Success)
+        {
+            Method = match.Groups[1].Value;
+            Path = match.Groups[2].Value;
+        };
+    }
+
+    public override string Key => "options-simple";
+    public override IOpenApiExtension Value
+    {
+        get
+        {
+            return new OpenApiObject
+            {
+                { "method", new OpenApiString(Method) },
+                { "path", new OpenApiString(Path) }
+            };
+        }
+    }
+}
